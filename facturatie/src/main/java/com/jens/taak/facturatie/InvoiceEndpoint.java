bfc3c5e99040eed7800c07d89ea5c72015518659
JensSanen <145580938+JensSanen@users.noreply.github.com>
2024-12-25 10:25:35 +0100

    Facturatie update 2

--- facturatie/src/main/java/com/jens/taak/facturatie/InvoiceEndpoint.java
+++ facturatie/src/main/java/com/jens/taak/facturatie/InvoiceEndpoint.java
@@ -10,6 +10,9 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import java.util.Arrays;
+import java.util.List;
+
 import taak.jens.com.facturatie.GetInvoiceRequest;
 import taak.jens.com.facturatie.GetInvoiceResponse;
 import taak.jens.com.facturatie.Invoice;
@@ -20,48 +23,51 @@ import taak.jens.com.facturatie.Phase;
 public class InvoiceEndpoint {
 
     private static final String NAMESPACE_URI = "http://com.jens.taak/facturatie";
-    private static final String API_URL = "http://localhost:30001/api/project/";
+    private static final String API_PROJECT_URL = "http://localhost:30001/api/project/";
 
     private RestTemplate restTemplate;
     private ObjectMapper jsonMapper;
-    private XmlMapper xmlMapper;
 
     @Autowired
     public InvoiceEndpoint(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
         this.jsonMapper = new ObjectMapper();
-        this.xmlMapper = new XmlMapper();
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getInvoiceRequest")
     @ResponsePayload
     public GetInvoiceResponse getInvoice(@RequestPayload GetInvoiceRequest request) throws Exception {
         int projectId = request.getProjectId();
-        System.out.println("Project ID: " + projectId);
-        String url = UriComponentsBuilder.fromUriString(API_URL + projectId).toUriString();
-
-        // Stap 1: JSON ophalen van de API
-        String jsonResponse = restTemplate.getForObject(url, String.class);
-
-        System.out.println("JSON Response: " + jsonResponse);
-
-        // Stap 2: JSON naar objecten converteren (lijst van Invoice-objecten)
-        Project project = jsonMapper.readValue(jsonResponse, Project.class);
-        // Invoice invoice = jsonMapper.readValue(jsonResponse, Invoice.class);
-        Invoice invoice = new Invoice();
-        invoice.setProject(project);
-
-        System.out.println("Converted Invoice: " + invoice);
-
-        // Stap 3: XML converteren
-        String xmlResponse = xmlMapper.writeValueAsString(invoice);
-
-        System.out.println("Converted XML: " + xmlResponse);
-
-        // Stap 4: Response opbouwen
+        Invoice invoice = createInvoice(projectId);
         GetInvoiceResponse response = new GetInvoiceResponse();
         response.setInvoice(invoice);
         return response;
+    }
+
+    private Project createProject(int projectId) throws Exception {
+        String project_URL = UriComponentsBuilder.fromUriString(API_PROJECT_URL + projectId).toUriString();
+        String phases_URL = UriComponentsBuilder.fromUriString(project_URL + "/phases").toUriString(); 
+
+        // Stap 1: JSON ophalen van de API
+        String jsonResponseProject = restTemplate.getForObject(project_URL, String.class);
+        String jsonResponsePhases = restTemplate.getForObject(phases_URL, String.class);
+
+        // Stap 2: JSON naar objecten converteren 
+        Project project = jsonMapper.readValue(jsonResponseProject, Project.class);
+        Phase[] phasesArray = jsonMapper.readValue(jsonResponsePhases, Phase[].class);
+        List<Phase> phaseList = Arrays.asList(phasesArray);
+
+        // Voeg de nieuwe fases toe aan de bestaande lijst
+        project.getPhases().addAll(phaseList);
+
+        return project;
+    }
+
+    private Invoice createInvoice(int projectId) throws Exception {
+        Project project = createProject(projectId);
+        Invoice invoice = new Invoice();
+        invoice.setProject(project);
+        return invoice;
     }
 }
 
